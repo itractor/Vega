803afd4cf02a5feedc2044ddb7b4fc559ab59237
oebars <oebars@gmail.com>
2022-01-02 19:33:52 +0100

    add cors

--- src/main/java/com/vega/backend/StockInformationController.java
+++ src/main/java/com/vega/backend/StockInformationController.java
@@ -2,13 +2,11 @@ package com.vega.backend;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
+@CrossOrigin
 @RestController
 public class StockInformationController {
 
