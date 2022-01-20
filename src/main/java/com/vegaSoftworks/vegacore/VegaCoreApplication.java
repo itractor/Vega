1b538ea1ee06cd348c1c95efb4934e1833ecc248
oebars <oebars@gmail.com>
2022-01-20 22:59:16 +0100

    refactoring

---
+++ src/main/java/com/vegaSoftworks/vegacore/VegaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.vegaSoftworks.vegacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegaCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vegaSoftworks/vegacore/VegaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegaSoftworks.vegacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
